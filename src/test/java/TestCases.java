565f29ba0075e4f62df75423c62dc475872f1736
Akila Mendis <akila@oneit.com.au>
2024-07-07 19:12:22 +0530

    13. Maximizing the window

--- src/test/java/TestCases.java
+++ src/test/java/TestCases.java
@@ -1,14 +1,19 @@
-import com.microsoft.playwright.Browser;
-import com.microsoft.playwright.BrowserType;
-import com.microsoft.playwright.Page;
-import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.*;
+
+import java.awt.*;
 
 public class TestCases {
 
     public static void main(String[] args) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = screenSize.getWidth();
+        double height = screenSize.getHeight();
+        System.out.println(width+"----------"+height);
+
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        Page page =  browser.newPage();
+        BrowserContext browserContext =  browser.newContext(new Browser.NewContextOptions().setViewportSize((int)width,(int)height));
+        Page page =  browserContext.newPage();
         page.navigate("https://google.com");
         System.out.println(page.title());
         page.close();
