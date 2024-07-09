c95ee6c9da1b172e643037e8264b8d7e31839803
Akila Mendis <godomainz@gmail.com>
2024-07-09 15:08:07 +0530

    fixed locator issues

--- src/test/java/TestMouseOver.java
+++ src/test/java/TestMouseOver.java
@@ -10,7 +10,7 @@ public class TestMouseOver {
         Page page = browser.newPage();
         page.navigate("https://www.way2automation.com/");
         page.locator("text=All Courses").nth(0).hover();
-        page.locator("#menu-item-27580 #menu-item-27592").click();
+        page.locator("#menu-item-27580 #menu-item-27592 a").click();
 
         Thread.sleep(2000);
         page.close();
