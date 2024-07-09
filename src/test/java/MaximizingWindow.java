82f5bfff46d37cde335828c326241ce312634cc3
Akila Mendis <godomainz@gmail.com>
2024-07-09 09:11:42 +0530

    15. Another simple way of Maximizing the window

---
+++ src/test/java/MaximizingWindow.java
@@ -0,0 +1,18 @@
+import com.microsoft.playwright.*;
+
+import java.util.ArrayList;
+
+public class MaximizingWindow {
+    public static void main(String[] args) {
+        Playwright playwright = Playwright.create();
+        ArrayList<String> arguments = new ArrayList<String>();
+        arguments.add("--start-maximized");
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false).setArgs(arguments));
+        BrowserContext browsercontext = browser.newContext(new Browser.NewContextOptions().setViewportSize(null));
+        Page page = browsercontext.newPage();
+        page.navigate("https://google.com");
+        System.out.println(page.title());
+        page.close();
+        playwright.close();
+    }
+}
